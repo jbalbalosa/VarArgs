a5c0294ec1a73c549bab1c2eb01a64845a53a346
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-10-07 14:44:06 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String... args) {
+        System.out.println("Hello world again!");
+
+        String[] splitString = "Hello World again".split(" ");
+        printText(splitString);
+
+        System.out.println("_".repeat(20));
+        printText("Hello");
+
+        System.out.println("_".repeat(20));
+        printText("Hello","World","again");
+
+        System.out.println("_".repeat(20));
+        printText();
+
+        String[] sArray = {"first","second","third","fourth","fifth"};
+        System.out.println(String.join(",",sArray));
+    }
+
+    private static void printText(String... textList){
+
+        for (String t : textList){
+            System.out.println(t);
+        }
     }
 }
